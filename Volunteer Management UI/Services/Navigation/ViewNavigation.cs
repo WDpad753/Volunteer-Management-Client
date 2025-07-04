d9bf43e8af7c93551111fe9a09697b5d5af33346
Mohamed Ahmed <mohamed.ahmed@s3-id.com>
2025-07-04 11:35:07 +0100

    Made some changes in some projects and fixing the base class.

---
+++ Volunteer Management UI/Services/Navigation/ViewNavigation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UIBaseClass.MVVM;
+
+namespace Volunteer_Management_UI.Services.Navigation
+{
+    public class ViewNavigation : ViewModelBase
+    {
+
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+
+        public ViewNavigation()
+        {
+        }
+    }
+}
