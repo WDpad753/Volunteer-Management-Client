1d0c0e935cdbf81378e1dfc000d65795480acf6d
WDpad753 <WDpad753@Admin>
2025-07-06 18:30:43 +0100

    modified the Volunteer UI application project.

--- Volunteer Management UI/App.xaml.cs
+++ Volunteer Management UI/App.xaml.cs
@@ -1,6 +1,12 @@
-﻿using System.Configuration;
+﻿using BaseLogger;
+using BaseLogger.Models;
+using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Windows;
+using Volunteer_Management_UI.MVVM.Views.Login;
+using UtilityClass = BaseClass.MethodNameExtractor.FuncNameExtractor;
+
 
 namespace Volunteer_Management_UI
 {
@@ -9,6 +15,18 @@ namespace Volunteer_Management_UI
     /// </summary>
     public partial class App : Application
     {
-    }
+        string _configPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Config","App.config");
+        string _logPath = "";
 
+        public LogWriter logwriter;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            logwriter = new(_configPath,_logPath);
+            // This section is about selecting the View MainWindow and running the application:
+            MainWindow = new LoginWindow();
+            MainWindow.Show();
+            logwriter.LogWrite("Application is running now", this.GetType().Name, UtilityClass.GetMethodName(), MessageLevels.Debug);
+            base.OnStartup(e);
+        }
+    }
 }
