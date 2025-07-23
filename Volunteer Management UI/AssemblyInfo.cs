3c7af7b105963b50b5813d0cc9bebd7cb97b1a24
Mohamed Ahmed <mohamed.ahmed@s3-id.com>
2025-07-23 08:48:15 +0100

    Updated the project again.

--- Volunteer Management UI/AssemblyInfo.cs
+++ Volunteer Management UI/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.25204.87")]
+[assembly: AssemblyVersion("1.0.25204.89")]
 [assembly: AssemblyFileVersion("1.0.25204.10")]
