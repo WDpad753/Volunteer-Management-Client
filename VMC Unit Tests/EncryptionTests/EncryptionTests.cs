aa6aa4f3b38962c66fdb380e37842e33227c8c8f
Mohamed Ahmed <mohamed.ahmed@s3-id.com>
2025-08-18 15:34:20 +0100

    Updating the base class and doing encryption tests.

--- VMC Unit Tests/EncryptionTests/EncryptionTests.cs
+++ VMC Unit Tests/EncryptionTests/EncryptionTests.cs
@@ -3,10 +3,12 @@ using BaseClass.Base.Interface;
 using BaseClass.Config;
 using BaseClass.Helper;
 using BaseClass.JSON;
+using BaseClass.Model;
 using BaseLogger;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,10 +24,12 @@ namespace VMC_Unit_Tests.EncryptionTests
         private EnvFileHandler envFileHandler;
         private EnvHandler envHandler;
         private RegistryHandler registryHandler;
+        private EncryptionModel? _encModel;
         private string logpath;
         private string configPath;
         private string testValue;
         private bool importEncryptedData;
+        private bool importEncryptedTestData;
 
         [SetUp]
         public void Setup()
@@ -48,21 +52,38 @@ namespace VMC_Unit_Tests.EncryptionTests
                 ConfigPath = Mainconfigpath,
             };
 
+            _encModel = new EncryptionModel()
+            {
+                ConfigKey = "RegistryPath",
+                PathKey = ProtectedData.Protect(Encoding.UTF8.GetBytes("SOFTWARE\\AppTest"), null, DataProtectionScope.CurrentUser),
+                Key = ProtectedData.Protect(Encoding.UTF8.GetBytes("Test"), null, DataProtectionScope.CurrentUser),
+                Keys = new List<byte[]>() { ProtectedData.Protect(Encoding.UTF8.GetBytes("Test"), null, DataProtectionScope.CurrentUser) },
+                EnvType = ProtectedData.Protect(ProtectedData.Protect(Encoding.UTF8.GetBytes("None"), null, DataProtectionScope.CurrentUser), null, DataProtectionScope.CurrentUser),
+                RegType = ProtectedData.Protect(Encoding.UTF8.GetBytes("User"), null, DataProtectionScope.CurrentUser),
+            };
+
             configReader = new(baseConfig);
             jsonFileHandler = new(baseConfig);
             envFileHandler = new(baseConfig);
             envHandler = new(baseConfig);
-            registryHandler = new(baseConfig);
+            registryHandler = new(baseConfig, _encModel);
             baseConfig.ConfigHandler = configReader;
             baseConfig.JSONFileHandler = jsonFileHandler;
             baseConfig.EnvFileHandler =  envFileHandler;
             baseConfig.EnvHandler = envHandler;
             baseConfig.RegistryHandler = registryHandler;
 
-            if(importEncryptedData)
+            testValue = "TestPath";
+
+            if (importEncryptedData)
             {
                 ConvertStringToBytes();
                 importEncryptedData = false;
+            }
+            else if(importEncryptedTestData)
+            {
+                testValue = Convert.ToBase64String(ProtectedData.Protect(Encoding.UTF8.GetBytes("SOFTWARE\\AppTest"), null, DataProtectionScope.CurrentUser));
+                ConvertStringToBytesReg();
             }
             else
             {
@@ -89,26 +110,52 @@ namespace VMC_Unit_Tests.EncryptionTests
             }
         }
 
-        //[Test, Order(1)]
-        //public void UserRegistryPathReadTest2()
-        //{
-        //    string val = "TestPath";
+        [Test, Order(1)]
+        public void UserRegistryPathReadTest2()
+        {
+            string val = "TestPath";
 
-        //    string? res = registryHandler.RegistryRead(Convert.FromBase64String("RegistryPath"));
+            string? res = registryHandler.RegistryRead("RegistryPath");
 
-        //    if (res != null)
-        //    {
-        //        Assert.That(res == val, "Value is not equal");
-        //    }
-        //    else
-        //    {
-        //        Assert.Fail("Unable to Obtain a Value from Configuration File");
-        //    }
-        //}
+            if (res != null)
+            {
+                Assert.That(res == val, "Value is not equal");
+                importEncryptedTestData = true;
+            }
+            else
+            {
+                Assert.Fail("Unable to Obtain a Value from Configuration File");
+            }
+        }
+
+        [Test, Order(2)]
+        public void UserRegistryDataReadTest()
+        {
+            string val = "Hello_Test";
+
+            //string? res = registryHandler.RegistryRead("RegistryPath");
+            List<byte[]>? res = registryHandler.RegistryValGet();
+
+            //if (res != null)
+            //{
+            //    ;
+
+            //    Assert.That( == val, "Value is not equal");
+            //}
+            //else
+            //{
+            //    Assert.Fail("Unable to Obtain a Value from Configuration File");
+            //}
+        }
 
         private void ConvertStringToBytes()
         {
-            registryHandler.RegistrySave("RegistryPath", Convert.FromBase64String(testValue));
+            registryHandler.RegistrySave("RegistryPath", Encoding.UTF8.GetBytes(testValue));
+        }
+
+        private void ConvertStringToBytesReg()
+        {
+            registryHandler.RegistrySave("RegistryPath", testValue);
         }
 
         private void ConvertBytesToString()
